2a38598b144a95155de57248ab8fd2e4858c2d6c
santy <sr.santiochoa@gmail.com>
2021-01-28 14:57:57 +0000

    Crea grupos y asigna usuarios v1.0

--- run.py
+++ run.py
@@ -12,9 +12,9 @@ passwd = "P@ssw0rd588588"
 cloud_provider = "GAWS"
 project_name = "PLANIFAN"
 project_env = "PRE"
-sufix_group = "ADMINS"
-ou_cloud_provider = "Amazon"
+sufix_group = "ADMINS2"
 cn_group_name = cloud_provider + '-' + project_name + '-' + project_env + '-' + sufix_group
+ou_cloud_provider = "Amazon"
 
 ################ File to create group into AD
 file_loader = FileSystemLoader('templates')
@@ -28,20 +28,19 @@ f.write(output)
 f.close()
 ################
 
-# ####################### Crea GRUPO
-# bashCommand = 'ldapadd -H ldap://%s -x -w %s -D %s -f %s' %(adserv, passwd, userad, path)
-
-# proc = subprocess.Popen(
-#     ["/usr/bin/bash", "-c", bashCommand],
-#     stdin=subprocess.PIPE,
-#     stdout=subprocess.PIPE,
-# )
-# # msg = 'through stdin to stdout'.encode('utf-8')
-# # stdout_value = proc.communicate(msg)[0].decode('utf-8')
-# # print('pass through:', repr(stdout_value))
-# stdout_value = proc.communicate()[0].decode('utf-8')
+####################### Crea GRUPO
+bashCommand = 'ldapadd -H ldap://%s -x -w %s -D %s -f %s' %(adserv, passwd, userad, path)
+proc = subprocess.Popen(
+    ["/usr/bin/bash", "-c", bashCommand],
+    stdin=subprocess.PIPE,
+    stdout=subprocess.PIPE,
+)
+# msg = 'through stdin to stdout'.encode('utf-8')
+# stdout_value = proc.communicate(msg)[0].decode('utf-8')
 # print('pass through:', repr(stdout_value))
-# ####################### FIN Crea GRUPO
+stdout_value = proc.communicate()[0].decode('utf-8')
+print('pass through:', repr(stdout_value))
+####################### FIN Crea GRUPO
 
 
 #ldapsearch -H ldap://10.0.0.4:3268 -x -w P@ssw0rd588588 -D "azureuser@ansible.com" "(cn=azureuser)" dn

5ab102aef023400f4da8d9a4aac2a462a77326db
santy <sr.santiochoa@gmail.com>
2021-01-29 00:16:44 +0000

    Add getMask Function

--- creategroups.py
+++ creategroups.py
@@ -33,10 +33,24 @@ def runCommandV2(command):
     out = proc.communicate()[0]
     return (out)
 
+def getMask(argument): 
+    switcher = { 
+        32: "27", 
+        64: "26", 
+        128: "25",
+        256: "24",
+    } 
+    return switcher.get(argument, "nothing")
+
 ### Variables
 userad = "azureuser@ansible.com"
 adserv = "10.0.0.4"
 passwd = "P@ssw0rd588588"
+userinfoblox = "sanale"
+passinfoblox = "Mapfre.1234"
+hostinfoblox = "10.230.93.16"
+globalnetwork = "10.85.0.0/17"
+reservmask = getMask(32)
 pathFileCreateGroups = "/tmp/newgroups" +getTimeStamp() +".ldif"
 pathFileAddUsers = "/tmp/addusers" +getTimeStamp() +".ldif"
 ###
@@ -45,6 +59,9 @@ pathFileAddUsers = "/tmp/addusers" +getTimeStamp() +".ldif"
 commandCreateGroup = 'ldapadd -H ldap://%s -x -w %s -D %s -f %s' %(adserv, passwd, userad, pathFileCreateGroups)
 commandSearchUser = 'ldapsearch -H ldap://%s:3268 -x -w %s -D %s "(cn=usuario1)" dn | grep -iE "dn: cn=" | awk  \'{print $2}\'' %(adserv, passwd, userad)
 commandAddUserToGroup = 'ldapmodify -H ldap://%s -x -w %s -D %s -f %s' %(adserv, passwd, userad, pathFileAddUsers)
+commandReservIP = """
+curl -k -u %s:%s -X POST 'https://%s/wapi/v2.9.1/network?_return_fields=network,comment&_return_as_object=1' -H 'content-type: application/json' -d '{"network":"func:nextavailablenetwork:%s,MAPFRE_GLOBAL,%s","network_view":"MAPFRE_GLOBAL","comment":"AWSIBM"}' | grep '"network": ' | awk '{print $2}' | tr -d '"'
+""" %(userinfoblox, passinfoblox, hostinfoblox, globalnetwork, reservmask)
 ###
 
 renderCreateGroup = {
@@ -64,3 +81,5 @@ renderAddUser = {
 templateAddUser = createTemplate('templates','addusertogroup.ldif',renderAddUser)
 writeFile(pathFileAddUsers,templateAddUser)
 print(runCommandV2(commandAddUserToGroup))
+
+print(commandReservIP)
